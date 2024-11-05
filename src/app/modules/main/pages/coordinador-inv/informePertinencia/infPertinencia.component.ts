4874876ff6c3c2a0d4d67c118bea34652a5760a2
Santiago David <sdquishpe1@espe.edu.ec>
2024-11-05 08:03:34 -0500

    update

---
+++ src/app/modules/main/pages/coordinador-inv/informePertinencia/infPertinencia.component.ts
@@ -0,0 +1,17 @@
+import { NgOptimizedImage } from "@angular/common";
+import { Component, OnInit, Injectable } from '@angular/core';
+
+@Component({
+    selector: 'vex-creation-form',
+    templateUrl: './infPertinencia.component.html',
+    styleUrls: ['./creation-form.component.scss'],
+  })
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class InfPertinenciaComponent implements OnInit {
+    ngOnInit() {
+        
+    }
+
+  }
