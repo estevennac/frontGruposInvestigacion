d1b74f1b6071095fa9b6dadc4b65951b308a963a
Adrian Chin <aachin@espe.edu.ec>
2024-11-07 11:57:46 -0500

    AssetsDetails

---
+++ src/app/types/assetesDetails.types.ts
@@ -0,0 +1,13 @@
+export interface AssetesDetails {
+    idDetalles: number;
+    idReporte: number;
+    codigo?: String;
+    descripcion?: String;
+    fechaAdquisicion?: Date;
+    estadoActual?: String;
+    ubicacionActual?: String;
+    usuarioCreacion?: String;
+    fechaCreacionDetalle?: Date;
+    usuarioModificacionDetalle?: String;
+    fechaModificacionDetalle?: Date;
+}
