2fceeefa4e1321159de55a626484bec242ac0728
LizardoMTitua-aM <lmtituana2@espe.edu.ec>
2024-11-08 12:37:23 -0500

    ods

---
+++ src/app/modules/main/pages/administracion/ods/modal_ods.component.ts
@@ -0,0 +1,110 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { OdsService } from 'src/app/core/http/ods/ods.service';
+import { AuthService } from 'src/app/core/auth/services/auth.service';
+import { ODS } from 'src/app/types/ods.types';
+
+@Component({
+    selector: 'app-area',
+    templateUrl: './modal_ods.component.html',
+    styleUrls: ['../../../styles/modales.scss']
+})
+export class OdsControl implements OnInit {
+    currentUser: string;
+    currentDate: Date = new Date();
+    ods: ODS[] = [];
+    odsForm: FormGroup;
+    isSaved: boolean = false;
+    isLoading: boolean = false;
+    isEditing: boolean = false; // Variable para determinar si se está en modo edición
+
+    constructor(
+        private fb: FormBuilder,
+        private authService: AuthService,
+        public dialogRef: MatDialogRef<OdsControl>,
+        private odsService: OdsService,
+        @Inject(MAT_DIALOG_DATA) public data: any, // Datos que vienen del componente de la tabla
+    ) {}
+
+    ngOnInit(): void {
+        this.currentUser = this.authService.getUserName();
+
+        // Inicializar el formulario
+        this.odsForm = this.fb.group({
+            nombreOds: ['', Validators.required],
+            estado: [1, Validators.required],
+        });
+
+        // Si hay datos, significa que se está editando un dominio
+        if (this.data && this.data.ods) {
+            this.isEditing = true;
+            this.loadData(this.data.ods); // Cargar los datos del dominio para editar
+        }
+    }
+
+    // Cargar los datos del dominio académico para la edición
+    loadData(ods: ODS) {
+        this.odsForm.patchValue({
+            nombreOds: ods.ods,
+            estado: ods.descripcion
+        });
+    }
+
+    // Método para crear o actualizar dependiendo de la acción
+    save() {
+        if (this.odsForm.valid) {
+            this.isLoading = true; // Mostrar el spinner
+
+            if (this.isEditing) {
+                this.update();
+            } else {
+                this.create();
+            }
+        }
+    }
+
+    // Crear un nuevo dominio académico
+    create() {
+        const odsData: ODS = this.odsForm.value;
+        odsData.fechaCreacion = this.currentDate;
+        odsData.usuarioCreacion = this.currentUser;
+
+        this.odsService.create(odsData).subscribe(
+            () => {
+                console.log('Area creada correctamente');
+                this.isSaved = true;
+                this.isLoading = false; // Ocultar el spinner
+                this.dialogRef.close(true); // Cerrar el modal y retornar éxito
+            },
+            (error) => {
+                console.error('Error al crear el ods', error);
+                this.isLoading = false; // Ocultar el spinner
+            }
+        );
+    }
+
+    // Editar un dominio académico existente
+    update() {
+        const updatedData: ODS = this.odsForm.value;
+        updatedData.fechaModificacion = this.currentDate;
+        updatedData.usuarioModificacion = this.currentUser;
+
+        this.odsService.update(this.data.area.idArea, updatedData).subscribe(
+            () => {
+                console.log('Ods actualizado correctamente');
+                this.isSaved = true;
+                this.isLoading = false; // Ocultar el spinner
+                this.dialogRef.close(true); // Cerrar el modal y retornar éxito
+            },
+            (error) => {
+                console.error('Error al actualizar el area', error);
+                this.isLoading = false; // Ocultar el spinner
+            }
+        );
+    }
+
+    onClickClose(): void {
+        this.dialogRef.close();
+    }
+}
--- src/app/modules/main/pages/administracion/ods/ods.component.ts
+++ src/app/modules/main/pages/administracion/ods/ods.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { Ods } from "src/app/types/ods.types";
+import { ODS } from "src/app/types/ods.types";
+import { Router } from '@angular/router';
 import { OdsService } from "src/app/core/http/ods/ods.service";
 import { OdsControl } from './modal_ods.component';
 import { MatDialog } from "@angular/material/dialog";
@@ -9,28 +10,71 @@ import { MatDialog } from "@angular/material/dialog";
   styleUrls: ["../modulos.component.scss"],
 })
 export class OdsComponent implements OnInit {
-    ods: Ods[] = [];
+  ods: ODS[] = [];
 
-    constructor(private odsService: OdsService, private dialog: MatDialog) {}
-    private dialog: MatDialog;
-) {}
+  constructor(private router: Router, private odsService: MatDialog,
+    private dialog: MatDialog
+  ) {}
 
-ngOnInit(){
-  this.get();
-}
+  ngOnInit() {
+    this.get();
+  }
 
 get() {
-  this.odsService.getAll().subscribe((data)=>{
-    this.area = data;
+  this.odsService.getAll().subscribe((data) => {
+    this.ods = data;
   })
 }
 
-openDialog(area?: Ods): void {
+openDialog(ods ?: ODS): void {
   const dialogRef = this.dialog.open(OdsControl, {
     width: '50%',
     height: '70%',
-    data: { area } 
+    data: { ods }
+  });
+
+  DialogRef.afterClosed().subscribe((result) => {
+    if (result) {
+      this.get();
+    }
   });
 }
 
+edit(id: number) {
+  this.odsService.getById(id).subscribe(
+    (area: ODS) => {
+      this.openDialog(area); // Abrir el modal con los datos para editar
+    },
+    (error) => {
+      console.error('Error al obtener los detalles del area', error);
+    }
+  );
+}
+
+deletee(id: number) {
+  this.odsService.update(id, { estado: false }).subscribe(
+    () => {
+      console.log(`ODS con ID ${id} eliminado correctamente`);
+      this.get();
+    },
+    (error) => {
+      console.error('Error al eliminar el dominio académico', error);
+    }
+  );
+}
+
+active(id: number) {
+  this.odsService.update(id, { estado: true }).subscribe(
+    () => {
+      console.log(`ODS con ID ${id} eliminado correctamente`);
+      this.get();
+    },
+    (error) => {
+      console.error('Error al eliminar el dominio académico', error);
+    }
+  );
+}
+goBack() {
+  this.router.navigate(['main/admin']);
+}
 }
