440f19028f6939e1d6f3c41875cac2f7116de85a
LizardoMTitua-aM <lmtituana2@espe.edu.ec>
2024-11-08 13:40:54 -0500

    dtoAC, dtoC ts
    Interfaces

---
+++ src/app/types/dtoAnnualControl.types.ts
@@ -0,0 +1,24 @@
+export interface AnnualControl {
+    idPanelControl: number;
+    idPlanAnual: number;
+    objetivoAnual: string;
+    producto: string;
+    financiamiento: string;
+    monto: number;
+    presupuesto: string;
+    periodicidad: string;
+    fechaInicio: Date;
+    fechaFin: Date;
+    mediosVerificacion: string;
+    montoCertificado: number;
+    montoComprometido: number;
+    valorDevengado: number;
+    certificado: string;
+    fechaSeguimiento: Date;
+    montoDisponible: number;
+    cumplimiento: number;
+    usuarioCreacion: string;
+    fechaCreacion: Date;
+    usuarioModificacion: string;
+    fechaModificacion: Date;
+}
---
+++ src/app/types/dtoCompliance.ts
@@ -0,0 +1,10 @@
+export interface Compliance {
+    idReporteActividades: number;
+    idObjEspecifico: number;
+    verificable: string;
+    porcentaje: number;
+    usuarioCreacion: string;
+    fechaCreacion: Date;
+    usuarioModificacion: string;
+    fechaModificacion: Date;
+}	
